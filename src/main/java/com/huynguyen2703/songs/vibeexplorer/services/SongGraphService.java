f561c868cb6d2ba9dbcf198906e7a6053f3a155d
huynguyen2703 <huynguyenquoc2703@gmail.com>
2025-10-16 02:14:22 -0700

    Commited SongGraphService.java file that was not included in the previous push

--- src/main/java/com/huynguyen2703/songs/vibeexplorer/services/SongGraphService.java
+++ src/main/java/com/huynguyen2703/songs/vibeexplorer/services/SongGraphService.java
@@ -1,4 +1,36 @@
 package com.huynguyen2703.songs.vibeexplorer.services;
 
+import com.huynguyen2703.songs.vibeexplorer.models.SongGraph;
+import com.huynguyen2703.songs.vibeexplorer.repositories.SongGraphRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class SongGraphService {
+    private final SongGraphRepository songGraphRepository;
+
+    @Autowired
+    public SongGraphService(SongGraphRepository songGraphRepository) {
+        this.songGraphRepository = songGraphRepository;
+    }
+
+    public SongGraph saveSongGraph(SongGraph songGraph) {
+        return songGraphRepository.save(songGraph);
+    }
+
+    public Optional<SongGraph> findSongGraphById(Long id) {
+        return songGraphRepository.findById(id);
+    }
+
+    public List<SongGraph> findAllSongGraphs() {
+        return songGraphRepository.findAll();
+    }
+
+    public void deleteSongGraph(SongGraph songGraph) {
+        songGraphRepository.delete(songGraph);
+    }
 }
